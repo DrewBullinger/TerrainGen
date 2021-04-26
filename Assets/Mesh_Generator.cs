972337d4f9f34a2431a32a41ac6370718f6e9d41
Delaney Johns <delaneyajohns@gmail.com>
2021-04-26 15:35:37 -0500

    Island progress- depressed center

--- Assets/Mesh_Generator.cs
+++ Assets/Mesh_Generator.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 
@@ -43,7 +48,92 @@ public class Mesh_Generator : MonoBehaviour
 
                 float y = Mathf.PerlinNoise(x * perlinZoomFactor , z * perlinZoomFactor) * perlinScaleFactor;
 
-                vertices[i] = new Vector3(x,y,z);
+               
+                
+                Vector3 center = new Vector3(100f, .5f, 100f);
+                Vector3 point = new Vector3(x, y, z);
+                float distance = Mathf.Abs(Vector3.Distance(center, point));
+
+
+                float twoPi = Mathf.PI * 2;
+                float t = 0;
+                float phase = 0;
+                //noise max?
+                float noiseMax = 0;
+
+                List<Vector3> verticesInIsland = new List<Vector3>();
+
+                for (float angle = 0; angle < twoPi; angle +=  0.1f)
+                {
+                    //float xOffset = Mathf.Cos(angle);
+                    //float yOffset = Mathf.Sin(angle);
+
+                    float xOffset = scale(Mathf.Cos(angle + phase), -1, 1, 0, noiseMax);
+                    float yOffset = scale(Mathf.Sin(angle + phase), -1, 1, 0, noiseMax);
+                    
+                    float noise = Mathf.PerlinNoise(xOffset, yOffset);
+                    float radius = scale(0f, 1f, 50f, 100f, noise);
+                    float newX = radius * Mathf.Cos(angle);
+                    float newY = radius * Mathf.Sin(angle);
+   
+                    verticesInIsland.Add(new Vector3(newX, newY, z));
+
+                    Vector3 pointInCircle = new Vector3(newX, newY, z);
+
+                    float distance2 = Mathf.Abs(Vector3.Distance(point, pointInCircle));
+                    if (distance < 10f)
+                    {
+                        y *= .2f;
+                    }
+
+
+                    t += .1f;
+                }
+
+                phase += 0.003f;
+
+              
+
+                /*if (distance > 60)
+                {
+                    float chance = UnityEngine.Random.Range(0f, .6f);
+                    //if (chance > 0f)
+                    //{
+                        y *= chance;
+                    //}
+                }
+
+                if (distance > 80)
+                {
+                    float chance = UnityEngine.Random.Range(.2f, .5f);
+                    if (chance > 0f)
+                    {
+                        y *= chance;
+                    }
+                }
+
+
+                if (distance > 100)
+                  {
+                    float chance = UnityEngine.Random.Range(.5f, .9f);
+                    if (chance > 0f)
+                    {
+                        y *= chance;
+                    }
+                }
+
+
+
+                for (int k = 0; k < verticesInIsland.Count; k++)
+                {
+                    if (verticesInIsland.Contains(new Vector3(x,y,z)))
+                    {
+                        y *= .3f;
+                    }
+                }*/
+
+                vertices[i] = new Vector3(x, y, z);
+
                 i++;
             }
         }
@@ -73,6 +163,18 @@ public class Mesh_Generator : MonoBehaviour
 
     }
 
+
+    public float scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue)
+    {
+
+        float OldRange = (OldMax - OldMin);
+        float NewRange = (NewMax - NewMin);
+        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+
+        return (NewValue);
+    }
+
+
     //updates the mesh in Unity
     void UpdateMesh()
     {
@@ -83,6 +185,8 @@ public class Mesh_Generator : MonoBehaviour
 
         mesh.RecalculateNormals(); 
     }
+
+
 
 
     //draws verticies of the tris
